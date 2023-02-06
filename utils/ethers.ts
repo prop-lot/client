a8a2b0ff89909b58257707a166973944abaf990e
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-02-06 22:30:01 +0000

    hardcode infura provider

--- utils/ethers.ts
+++ utils/ethers.ts
@@ -1,7 +1,11 @@
 import { providers } from 'ethers'
 
-export const provider = new providers.JsonRpcProvider(process.env.JSON_RPC_CLIENT)
+export const provider = new providers.JsonRpcProvider("https://mainnet.infura.io/v3/4ab8992a9fc64db2b3f81bdd5d043f41")
 export const getBlock = async () => {
+  try {
     const blockNumber = await provider.getBlockNumber()
     return blockNumber
+  } catch (err) {
+    throw new Error('Failed to fetch current block')
+  }
 }
