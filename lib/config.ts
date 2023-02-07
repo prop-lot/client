b6d076745e7787e93e33ac7fe4425deca2c3ebd7
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-02-07 20:00:49 +0000

    cleanup

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,6 @@
-import getConfig from "next/config";
-const { serverRuntimeConfig } = getConfig();
-
-console.log('SERVER CONFIG', serverRuntimeConfig)
-console.log('ENV', process.env.SECRET_AUTH_PASSWORD)
-
 export const ironOptions = {
   cookieName: "proplot-auth",
-  password: process.env.SECRET_AUTH_PASSWORD || serverRuntimeConfig.SECRET_AUTH_PASSWORD || '',
+  password: process.env.SECRET_AUTH_PASSWORD || '',
   cookieOptions: {
     secure: process.env.NODE_ENV === 'production',
   },
