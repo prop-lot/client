64dd41e82a27f879f20877bb85cc4a29a53a88c0
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-02-07 19:25:26 +0000

    check runtime config

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,12 @@
+import getConfig from "next/config";
+const { serverRuntimeConfig } = getConfig();
+
+console.log(serverRuntimeConfig)
+console.log(process.env)
+
 export const ironOptions = {
   cookieName: "proplot-auth",
-  password: process.env.SECRET_AUTH_PASSWORD || 'test', // TODO: Replace with env var. Must be a 32 char long private key.
+  password: process.env.SECRET_AUTH_PASSWORD || serverRuntimeConfig.SECRET_AUTH_PASSWORD || 'test', // TODO: Replace with env var. Must be a 32 char long private key.
   cookieOptions: {
     secure: process.env.NODE_ENV === 'production',
   },
