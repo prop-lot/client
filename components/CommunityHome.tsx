79b03f8b94cca15b492af468a0ae564b398b737e
Michael Gingras <mcg79@cornell.edu>
2023-02-13 22:53:49 -0700

    actually implementing redirect to apply to all pages

--- components/CommunityHome.tsx
+++ components/CommunityHome.tsx
@@ -9,10 +9,8 @@ import IdeaRow from "@/components/IdeaRow";
 import UIFilter from "@/components/UIFilter";
 import useSyncURLParams from "@/hooks/useSyncURLParams";
 import EmptyState from "@/components/EmptyState";
-import { Community } from "@prisma/client";
-import NavBar from "@/components/Navbar";
 
-export default function CommunityHome({ community }: { community: Community }) {
+export default function CommunityHome() {
   const { address } = useAccount();
 
   const [getPropLotQuery, { data, refetch, error }] = useLazyQuery(
@@ -97,78 +95,75 @@ export default function CommunityHome({ community }: { community: Community }) {
   const nounBalance = getDelegatedVotesData?.delegate?.delegatedVotes || 0; // todo: replace
 
   return (
-    <>
-      <NavBar />
-      <main className="pt-8">
-        <section className="max-w-screen-xl mx-auto px-[20px] xl:px-0">
-          <div className="my-12 flex flex-row space-x-4 items-center">
-            <span className="w-52 h-52 border bg-gray-200 block rounded-lg"></span>
-            <h3 className="text-3xl font-bold">Nouns PropLot</h3>
-          </div>
-          <div className="flex flex-col-reverse sm:flex-row justify-between mb-4 items-start sm:items-center">
-            <div className="flex flex-row space-x-4">
-              {data?.propLot?.sortFilter && (
-                <UIFilter
-                  filter={data.propLot.sortFilter}
-                  updateFilters={handleUpdateFilters}
-                />
-              )}
-              {data?.propLot?.tagFilter && (
-                <UIFilter
-                  filter={data.propLot.tagFilter}
-                  updateFilters={handleUpdateFilters}
-                />
-              )}
-              {data?.propLot?.dateFilter && (
-                <UIFilter
-                  filter={data.propLot.dateFilter}
-                  updateFilters={handleUpdateFilters}
-                />
-              )}
-            </div>
-            <button
-              className={`${
-                nounBalance > 0
-                  ? "!bg-[#2B83F6] !text-white"
-                  : "!bg-[#F4F4F8] !text-[#E2E3E8]"
-              } !border-none !text-[16px] !rounded-[10px] !font-propLot !font-bold !pt-[8px] !pb-[8px] !pl-[16px] !pr-[16px] mb-[8px] sm:mb-0`}
-              onClick={() => {
-                if (nounBalance > 0) {
-                  Router.push("/idea/new");
-                }
-              }}
-            >
-              New Submission
-            </button>
-          </div>
-        </section>
-
-        <section className="border-t bg-gray-100 pb-8 px-[20px] xl:px-0">
-          <div className="max-w-screen-xl mx-auto pt-8 space-y-4">
-            {data?.propLot?.ideas?.map((idea: any, idx: number) => {
-              return (
-                <IdeaRow
-                  key={`idea-${idx}`}
-                  idea={idea}
-                  nounBalance={nounBalance}
-                  refetch={() => {
-                    handleRefresh();
-                  }}
-                />
-              );
-            })}
-            {data?.propLot?.ideas?.length === 0 && (
-              <EmptyState
-                appliedFilters={appliedFilters}
-                error={error}
-                clearFilters={() => {
-                  refetch({ options: { requestUUID: v4(), filters: [] } });
-                }}
+    <main className="pt-8">
+      <section className="max-w-screen-xl mx-auto px-[20px] xl:px-0">
+        <div className="my-12 flex flex-row space-x-4 items-center">
+          <span className="w-52 h-52 border bg-gray-200 block rounded-lg"></span>
+          <h3 className="text-3xl font-bold">Nouns PropLot</h3>
+        </div>
+        <div className="flex flex-col-reverse sm:flex-row justify-between mb-4 items-start sm:items-center">
+          <div className="flex flex-row space-x-4">
+            {data?.propLot?.sortFilter && (
+              <UIFilter
+                filter={data.propLot.sortFilter}
+                updateFilters={handleUpdateFilters}
+              />
+            )}
+            {data?.propLot?.tagFilter && (
+              <UIFilter
+                filter={data.propLot.tagFilter}
+                updateFilters={handleUpdateFilters}
+              />
+            )}
+            {data?.propLot?.dateFilter && (
+              <UIFilter
+                filter={data.propLot.dateFilter}
+                updateFilters={handleUpdateFilters}
               />
             )}
           </div>
-        </section>
-      </main>
-    </>
+          <button
+            className={`${
+              nounBalance > 0
+                ? "!bg-[#2B83F6] !text-white"
+                : "!bg-[#F4F4F8] !text-[#E2E3E8]"
+            } !border-none !text-[16px] !rounded-[10px] !font-propLot !font-bold !pt-[8px] !pb-[8px] !pl-[16px] !pr-[16px] mb-[8px] sm:mb-0`}
+            onClick={() => {
+              if (nounBalance > 0) {
+                Router.push("/idea/new");
+              }
+            }}
+          >
+            New Submission
+          </button>
+        </div>
+      </section>
+
+      <section className="border-t bg-gray-100 pb-8 px-[20px] xl:px-0">
+        <div className="max-w-screen-xl mx-auto pt-8 space-y-4">
+          {data?.propLot?.ideas?.map((idea: any, idx: number) => {
+            return (
+              <IdeaRow
+                key={`idea-${idx}`}
+                idea={idea}
+                nounBalance={nounBalance}
+                refetch={() => {
+                  handleRefresh();
+                }}
+              />
+            );
+          })}
+          {data?.propLot?.ideas?.length === 0 && (
+            <EmptyState
+              appliedFilters={appliedFilters}
+              error={error}
+              clearFilters={() => {
+                refetch({ options: { requestUUID: v4(), filters: [] } });
+              }}
+            />
+          )}
+        </div>
+      </section>
+    </main>
   );
 }
