33bff1cd804a361ddb55b571f78126c9ce5d051b
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-02-14 20:58:15 +0000

    Update graphql middleware to load community data and add it to context

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,6 +4,8 @@ import { ironOptions } from "@/lib/config";
 
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { SUPPORTED_SUBDOMAINS, SupportedTokenGetterMap } from "@/utils/supportedTokenUtils";
+import getCommunityByDomain from "@/utils/communityByDomain";
 
 // @ts-ignore
 const server = new ApolloServer({
@@ -13,11 +15,30 @@ const server = new ApolloServer({
 export default withIronSessionApiRoute(
   startServerAndCreateNextHandler(server, {
     context: async (req) => {
+      const { communityDomain, supportedTokenConfig } = getCommunityByDomain(req);
+
+      // We could avoid doing this lookup and just hardcode the community id in
+      // the supportedTokenConfig for each community but this is a bit more
+      // correct.
+      const community = await prisma.community.findFirst({
+        where: {
+          uname: communityDomain.toLowerCase(),
+        },
+      });
+
+      // If there is no subdomain then don't throw an error here as we're on
+      // proplot.wtf and may want to handle API calls differently.
+      if (communityDomain && !supportedTokenConfig) {
+        throw new Error('This community does not exist');
+      }
+
       return {
         authScope: Boolean(req.session.user?.wallet)
           ? { user: req.session.user, isAuthorized: true }
           : { isAuthorized: false, user: {} },
         timeZone: req.headers["proplot-tz"] || "UTC",
+        communityId: community?.id,
+        communityTokenConfig: supportedTokenConfig,
       };
     },
   }),
