44f000f70636f64878539439f3aa68c6c72effb1
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-07-20 22:54:33 +0100

    Add route for ideas list

---
+++ pages/idea/index.tsx
@@ -0,0 +1,47 @@
+import LandingPage from "@/components/LandingPage";
+import IdeasHome from "@/components/IdeasHome";
+import { GetServerSidePropsContext } from "next";
+import { Community } from "@prisma/client";
+import prisma from "@/lib/prisma";
+import getCommunityByDomain from "@/utils/communityByDomain";
+
+export const DEFAULT_HOMEPAGE_MATCH = "__NONE__";
+
+const IdeasPage = ({
+  community,
+}: {
+  community: Community & { data: { name: string; pfpUrl: string } };
+}) => {
+  return <IdeasHome community={community} />;
+};
+
+export default IdeasPage;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  let community;
+  const { communityDomain } = getCommunityByDomain(context.req);
+
+  if (communityDomain) {
+    community = await prisma.community.findFirst({
+      where: {
+        uname: communityDomain,
+      },
+    });
+  } else {
+    return {
+      notFound: true,
+    };
+  }
+
+  if (!community) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      community: JSON.parse(JSON.stringify(community)),
+    },
+  };
+}
