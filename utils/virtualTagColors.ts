603f928dbaaab7be23133f394b149f66145ea550
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-08-21 22:30:29 +0100

    Update virtual tag configuration

--- utils/virtualTagColors.ts
+++ utils/virtualTagColors.ts
@@ -1,6 +1,46 @@
-export const virtualTagColorMap: { [key: string]: string } = {
-  NEW: "bg-[#FCA33A] text-white",
-  DISCUSSION: "bg-[#F65874] text-white",
-  CONSENSUS: "bg-[#7B61FF] text-white",
-  CLOSED: "bg-[#8C8D92] text-white",
+export const virtualTagColorMap: { [key: string]: { colors: string; logo?: string; } } = {
+  NEW: {
+    colors: "bg-[#FCA33A] text-white",
+    logo: undefined,
+  },
+  DISCUSSION: {
+    colors: "bg-[#F65874] text-white",
+    logo: undefined,
+  },
+  CONSENSUS: {
+    colors: "bg-[#7B61FF] text-white",
+    logo: undefined,
+  },
+  CLOSED: {
+    colors: "bg-[#8C8D92] text-white",
+    logo: undefined,
+  },
+  CREATIVE: {
+    colors: "text-blue bg-blue/20",
+    logo: '/brush.svg',
+  },
+  COMMUNITY: {
+    colors: "text-pink bg-pink/20",
+    logo: '/heart.svg',
+  },
+  GOVERNANCE: {
+    colors: "text-yellow bg-yellow/20",
+    logo: '/ticket-alt.svg',
+  },
+  PUBLIC_GOOD: {
+    colors: "text-green bg-green/20",
+    logo: '/globe.svg',
+  },
+  SOFTWARE: {
+    colors: "text-light-purple bg-light-purple/20",
+    logo: '/code.svg',
+  },
+  HARDWARE: {
+    colors: "text-orange bg-orange/20",
+    logo: '/gear.svg',
+  },
+  OTHER: {
+    colors: "text-purple bg-purple/20",
+    logo: '/question.svg',
+  },
 };
