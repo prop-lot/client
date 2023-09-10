eba0fb08d5f1d842231309db33e52b3d4ddc19ca
Michael Gingras <mcg79@cornell.edu>
2023-09-09 21:40:18 -0600

    Hardcodes community domain

--- utils/communityByDomain.tsx
+++ utils/communityByDomain.tsx
@@ -4,21 +4,29 @@ import {
 } from "./supportedTokenUtils";
 
 const getCommunityByDomain = (req: any) => {
-  const host = req.headers.host;
-  const cdHeader = req.headers["proplot-cd"]; // used for local dev only where SSR requests are on the root domain.
-  const isDev = process.env.NODE_ENV === "development";
-  const subdomain = isDev
-    ? host?.match(/(.*)\.localhost:3000/)
-    : host?.match(/(.*)\.proplot\.wtf/);
+  // const host = req.headers.host;
+  // const cdHeader = req.headers["proplot-cd"]; // used for local dev only where SSR requests are on the root domain.
+  // const isDev = process.env.NODE_ENV === "development";
+  // const subdomain = isDev
+  //   ? host?.match(/(.*)\.localhost:3000/)
+  //   : host?.match(/(.*)\.proplot\.wtf/);
 
-  const communityDomain =
-    subdomain?.[1]?.toLowerCase() || (isDev && cdHeader?.toLowerCase()) || "";
-  const supportedTokenConfig =
-    SupportedTokenGetterMap[communityDomain as SUPPORTED_SUBDOMAINS];
+  // const communityDomain =
+  //   subdomain?.[1]?.toLowerCase() || (isDev && cdHeader?.toLowerCase()) || "";
+
+  // const communityDomain = "lilnouns";
+
+  // const supportedTokenConfig =
+  //   SupportedTokenGetterMap[communityDomain as SUPPORTED_SUBDOMAINS];
+
+  // return {
+  //   communityDomain,
+  //   supportedTokenConfig,
+  // };
 
   return {
-    communityDomain,
-    supportedTokenConfig,
+    communityDomain: "lilnouns",
+    supportedTokenConfig: SupportedTokenGetterMap["lilnouns"],
   };
 };
 
